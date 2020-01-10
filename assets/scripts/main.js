11d74c03058d49d8f0c0034dc1a4c4b00d934256
Henric <henric.bjorkvall@gmail.com>
2020-01-10 09:40:20 +0100

    fixed js console error

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,13 +2,13 @@ const settingsButton = document.querySelector(".settingsButton");
 const settings = document.querySelector(".settings");
 const followButton = document.querySelector(".followButton");
 
-settingsButton.addEventListener("click", event => {
-  settings.classList.toggle("change");
-});
-
-settingsButton.addEventListener("click", event => {
-  settingsButton.classList.toggle("rotate");
-});
-
-
-
+if (settingsButton != null) {
+  settingsButton.addEventListener("click", event => {
+    settings.classList.toggle("change");
+  });
+}
+if (settingsButton != null) {
+  settingsButton.addEventListener("click", event => {
+    settingsButton.classList.toggle("rotate");
+  });
+}
