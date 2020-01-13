8cd7773b8ecd3a7f8c0ca19b81d80df6d28c73f4
Henric <henric.bjorkvall@gmail.com>
2020-01-13 09:20:09 +0100

    additional default message styling

--- default.php
+++ default.php
@@ -1,3 +1,5 @@
 <?php require __DIR__ . '/views/header.php' ?>
 <?php authenticateUser(); ?>
-<p>Page does not exist. Go back to <a href="/index.php">Picture This</a></p>
+<div class="defaultMessageContainer">
+    <p class="defaultMessage">Page does not exist. Go back to <a class="defaultMessageLink" href="/index.php">Picture This</a></p>
+</div>
